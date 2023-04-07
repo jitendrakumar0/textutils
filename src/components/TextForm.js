bef6978a98f2bbc1074f658c44b21badb9030b4d
jitendrakumar0 <70063345+jitendrakumar0@users.noreply.github.com>
2023-04-07 09:23:26 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,6 +8,21 @@ export default function TextForm(props) {
     let newText = text.toUpperCase();
     setText(newText);
   }
+  const handleLowerClick = () => {
+    console.log('Lowercase was clicked' + text);
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
+  
+  const handleCapClick = () => {
+    const capitalize = str => {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    };
+    
+    console.log('Capatalize was clicked' + text);
+    let newText = capitalize(text);
+    setText(newText);
+  }
   const handleOnChange = (event) => {
     console.log('onChange');
     setText(event.target.value);
@@ -20,7 +35,9 @@ export default function TextForm(props) {
       <label htmlFor="myBox" className="form-label h4">{props.heading}</label>
       <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="8"></textarea>
     </div>
-    <button className="btn btn-primary text-white" onClick={handleUpClick}>Convert to Uppercase</button>
+    <button className="btn btn-primary me-2 text-white" onClick={handleUpClick}>Uppercase</button>
+    <button className="btn btn-primary me-2 text-white" onClick={handleLowerClick}>Lowercase</button>
+    <button className="btn btn-primary me-2 text-white" onClick={handleCapClick}>Capatalize</button>
     </>
   )
 }
